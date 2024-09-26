22132e4e9a3fd9d7fa85a8dd4aab71afcf4474eb
Tommy Wu <wu.tommy@gmail.com>
2024-09-26 09:33:51 +0800

    init
    Change-Id: Id194ee2a6524842c26cca9f0d0c31f3687fa8d44

---
+++ app/src/main/java/com/twu2/chinesecontactsearchfix/MainHook.java
@@ -0,0 +1,22 @@
+package com.twu2.chinesecontactsearchfix;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.android.providers.contacts")) return;
+        XposedHelpers.findAndHookMethod("com.android.providers.contacts.LocaleSet",
+                lpparam.classLoader,
+                "shouldPreferSimplifiedChinese",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        param.setResult(true);
+                    }
+                });
+    }
+}
